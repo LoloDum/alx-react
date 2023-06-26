54e4a8157730ee8a3b457c783739e02767fc6c86
LoloDum <chinedumokamgba@gmail.com>
2023-06-26 09:56:53 +0000

    index.js

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import App from "./App/App";
-import Notifications from "./Notifications/Notifications";
+import uiReducer, { initialState } from "./reducers/uiReducer";
+import { Map } from "immutable";
+
+const store = createStore(uiReducer, Map(initialState));
 
 ReactDOM.render(
   <React.StrictMode>
-    <div id="root-notifications">
-      <Notifications />
-    </div>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
